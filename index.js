f06ff08636bb1d47740ed908152f3d70d842cb19
Jay Sharma <jayrocks3112000@gmail.com>
2021-11-22 08:43:08 -0800

    second commit

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv/config')
+const cors=require('cors')
 // const bodyparser=require('body-parser')
 const app=express();
 const authroute=require('./routes/auth')
+const postroute=require('./routes/posts')
+const userroute=require('./routes/user')
+const multer = require("multer");
 app.use(express.json())
+app.use(cors())
 const DB=process.env.DB_data
 
 // app.use(bodyparser.json())
@@ -17,9 +22,28 @@ mongoose.connect(DB).then(()=>
 })
 
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    try {
+      return res.status(200).json("File uploded successfully");
+    } catch (error) {
+      console.error(error);
+    }
+  });
 
 
  app.use('/api/user',authroute)
+ app.use('/api/post',postroute)
+ app.use('/api/users',userroute)
 
 app.get('/',(req,res)=>{
     res.send('login/signup code')
